0bbd87e079319f361b852a4f421a654eba55dca8
Diana Bouquet <bouquetdiana@gmail.com>
2022-06-14 19:11:02 -0300

    agregar y modificar una novedad

--- M3U3 sitio ICE/models/novedadesModel.js
+++ M3U3 sitio ICE/models/novedadesModel.js
@@ -1,6 +1,6 @@
-var pool = require ('./bd')
+var pool = require('./bd')
 
-async function getNovedades () {
+async function getNovedades() {
     var query = "select * from novedades";
     var rows = await pool.query(query);
     return rows;
@@ -12,4 +12,31 @@ async function deleteNovedadesbyId(id) {
     return rows;
 }
 
-module.exports = { getNovedades, deleteNovedadesbyId }
+async function insertNovedad(obj) {
+    try {
+        var query = "insert into novedades set ?";
+        var rows = await pool.query(query, [obj]);
+        return rows;
+    } catch (error) {
+      //*  console.log(error);
+        throw error;
+    }
+}
+
+async function getNovedadbyId(id) {
+    var query = "select * from novedades where id = ?";
+    var rows = await pool.query(query, [id]);
+    return rows[0];
+}
+
+async function modificarNovedadById(obj, id) {
+    try {
+        var query = "update novedades set ? where id = ?";
+        var rows = await pool.query(query, [obj, id]);
+        return rows;
+    } catch (error) {
+        throw error;
+    }
+}
+
+module.exports = { getNovedades, deleteNovedadesbyId, insertNovedad, getNovedadbyId, modificarNovedadById }
